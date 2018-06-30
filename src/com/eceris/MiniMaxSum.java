c757b6bd8e96daad23fd12da21b18af68d8779bc
eceris <eceris@gmail.com>
2018-06-30 14:10:45 +0900

    solve MiniMaxSum

---
+++ src/com/eceris/MiniMaxSum.java
@@ -0,0 +1,23 @@
+package com.eceris;
+
+//https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+public class MiniMaxSum {
+    public static void main(String[] args) {
+        int [] arr = {1, 2, 3, 4, 5,};
+
+        long sum = 0;
+        long min = Long.MAX_VALUE;
+        long max = Long.MIN_VALUE;
+        for(int i : arr) {
+            sum += i;
+            if (i > max) {
+                max = i;
+            }
+            if (i < min) {
+                min = i;
+            }
+        }
+        System.out.println((sum - max) + " " + (sum - min));
+    }
+}
