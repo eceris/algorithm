420d17db241e89d29e8e9156dbdb9069ae02c3ea
eceris <eceris@daou.co.kr>
2018-07-02 08:51:37 +0900

    solve BetweenTwoSets

--- src/com/eceris/BetweenTwoSets.java
+++ src/com/eceris/BetweenTwoSets.java
@@ -1,16 +1,17 @@
 package com.eceris;
 
-import java.net.ConnectException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 //https://www.hackerrank.com/challenges/between-two-sets/problem
 public class BetweenTwoSets {
     public static void main(String[] args) {
         int[] a = {2, 4};
         int[] b = {16, 32, 96};
+
+        //요 케이스는 잘 모르것다 ...ㅠㅠ
+//        int[] a = {1};
+//        int[] b = {100};
 
         int totalX = getTotalX(a, b);
         System.out.println(totalX);
@@ -20,55 +21,57 @@ public class BetweenTwoSets {
         /*
          * Write your code here.
          */
-        HashSet<Integer> commonMultiple = getCommonMultiple(a);
+        List<Integer> multiple = getMultiple(a);
         HashSet<Integer> commonFactor = getCommonFactor(b);
 
         Iterator<Integer> iterator = commonFactor.iterator();
         int count = 0;
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             Integer next = iterator.next();
-            if(commonMultiple.contains(next)) {
-                System.out.println(next);
+            if (multiple.contains(next)) {
                 count++;
             }
         }
         return count;
     }
 
-    static HashSet<Integer> getCommonMultiple(int a[]) {
-        HashSet<Integer> commonMultiple = new HashSet<>();
-        for(int i : a) {
-            commonMultiple.addAll(getMultiple(i));
-        }
-        return commonMultiple;
-    }
-    static List<Integer> getMultiple(int v) {
+    //공배수
+    static List<Integer> getMultiple(int [] a) {
         List<Integer> multiples = new ArrayList<>();
-        for (int i = 1; i <= 100; i++) {
-            multiples.add(i*v);
+        for (int i = 1; i <= 100 ; i++) {
+            int count =0;
+            for(int v : a) {
+                if (i % v ==0) {
+                    count++;
+                }
+                if (count == a.length) {
+                    multiples.add(i);
+                }
+            }
         }
         return multiples;
     }
-    //약수
+
+    //공약수
     static HashSet<Integer> getCommonFactor(int b[]) {
+        List<Integer> collect = Arrays.stream(b).boxed().collect(Collectors.toList());
+        collect.sort((o1, o2) -> 0);
+        Integer last = collect.get(collect.size() - 1);
         HashSet<Integer> commonFactor = new HashSet<>();
-        for(int i : b) {
-            commonFactor.addAll(getFactor(i));
-        }
-        return commonFactor;
-    }
-    static List<Integer> getFactor(int v) {
-        List<Integer> factors = new ArrayList<>();
-        for (int i = 1; i <= v; i++) {
-            if (v % i ==0) {
-                factors.add(i);
+        for (int i = 1; i <= last; i++) {
+            int count = 0;
+            int temp = 0;
+            for (Integer integer : collect) {
+                if (integer % i == 0) {
+                    temp = i;
+                    count++;
+                }
+            }
+
+            if (count == collect.size()) {
+                commonFactor.add(temp);
             }
         }
-        return factors;
-    }
-    static void check(HashSet<Integer> commonFactor, List<Integer> factors) {
-        for (Integer i : factors) {
-
-        }
+        return commonFactor;
     }
 }
