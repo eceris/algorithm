47ec3eee75be49e41b9d9c50bb5d43317501108b
eceris <eceris@daou.co.kr>
2018-07-03 09:42:38 +0900

    solve BirthdayChocolate

--- src/com/eceris/BirthdayChocolate.java
+++ src/com/eceris/BirthdayChocolate.java
@@ -1,18 +1,14 @@
 package com.eceris;
 
-//https://www.hackerrank.com/challenges/the-birthday-bar/problem
-
-import java.util.Arrays;
-import java.util.HashSet;
+//  https://www.hackerrank.com/challenges/the-birthday-bar/problem
 
 public class BirthdayChocolate {
-    public static HashSet<String> VALUE = new HashSet<>();
 
     public static void main(String[] args) {
-        int[] s = {2, 2, 1, 3, 2}; //2
-        int d = 4, m = 2;
-        int solve = solve(s, d, m);
-        System.out.println(solve);
+
+//        int[] s = {2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1}; //3
+//        int d = 18, m = 7;
+
 //        int [] s = {1, 2, 1, 3, 2}; //2
 //        int d = 3, m = 2;
 //
@@ -21,57 +17,28 @@ public class BirthdayChocolate {
 //
 //        int [] s = {4}; //1
 //        int d = 4, m = 1;
+
+        int[] s = {2, 2, 1, 3, 2}; //2
+        int d = 4, m = 2;
+        int solve = solve(s, d, m);
+        System.out.println(solve);
+
     }
 
     // Complete the solve function below.
     static int solve(int[] s, int d, int m) {
-        int data[] = new int[m];
-        combination(s, data, 0, s.length - 1, 0, m, d);
-        return VALUE.size();
-    }
-
-
-    /**
-     * @param arr   원소들
-     * @param temp  뽑힌 원소
-     * @param start 원소 탐색 시작지점
-     * @param end   원소 탐색 종료지점
-     * @param index 현재 뽑은 갯수
-     * @param pick  뽑아야할 갯수
-     */
-//    static void combination(int arr[]/*원소들*/, int temp[]/*뽑힌원소*/, int start/*시작지점*/, int end/*종료지점*/, int index/*현재뽑은갯수*/, int pick/*뽑아야할갯수*/) {
-    static void combination(int arr[], int temp[], int start, int end, int index, int pick, int day) {
-        //r개만큼 모두 뽑은 경우
-        if (index == pick) {
-            int value = Arrays.stream(temp).reduce((a, b) -> a + b).getAsInt();
-            if (value == day) {
-                VALUE.add(addSet(temp));
+        int sum[] = new int[105];
+        int count = 0;
+        sum[0] = 0;
+        // sum0 =0, sum1 = 0 +s0, sum2= s0 + s1, sum3 = s0 + s1 + s2, .....
+        for (int i = 0; i < s.length; i++) {
+            sum[i + 1] = sum[i] + s[i];
+        }
+        for (int i = 0; i <= s.length - m; i++) {
+            if (sum[i + m] - sum[i] == d) {
+                count++;
             }
-//            print(temp);
-            return;
         }
-
-        for (int i = start; i <= end && end - start + 1 >= pick - index; i++) {
-            //원소 1개 고정
-            temp[index] = arr[i];
-            //재귀
-            combination(arr, temp, i + 1, end, index + 1, pick, day);
-        }
+        return count;
     }
-
-    static String addSet(int temp[]) {
-        StringBuffer sb = new StringBuffer();
-        for (int i : temp) {
-            sb.append(i);
-        }
-        return sb.toString();
-    }
-
-    static void print(int temp[]) {
-        for (int i : temp) {
-            System.out.print(i);
-        }
-        System.out.println();
-    }
-
 }
