35493f41bf5dadc936b0584aee6c5bdb1954a21b
eceris <eceris@gmail.com>
2018-07-05 19:39:14 +0900

    solve SuperReducedString

--- src/com/eceris/String/SuperReducedString.java
+++ src/com/eceris/String/SuperReducedString.java
@@ -1,80 +1,35 @@
 package com.eceris.String;
 //    https://www.hackerrank.com/challenges/reduced-string/problem
 
-import java.util.*;
+import java.util.Stack;
 
 public class SuperReducedString {
     public static void main(String[] args) {
-        String[] split = "abba".split("");
-        List<String> target = Arrays.asList(split);
-
-        int index = 0;
-        while (true) {
-            if (isRightDuplicated(target, index)) {
-                target.set(index, "0");
-                target.set(index+1, "0");
-            }
-            if (isLeftDuplicated(target, index)) {
-                target.set(index-1, "0");
-                target.set(index, "0");
-            }
-            index++;
-            List<String> temp = vacuum(target);
-            target = temp;
-        }
-    }
-
-    // 0 을 제거
-    static List<String> vacuum(List<String> target) {
-        List<String> result = new ArrayList<>();
-        for(String s: target) {
-            if (!s.equals("0")) {
-                result.add(s);
-            }
-        }
-        return result;
-    }
-    static boolean isLeftDuplicated(List<String> target, int index) {
-        if (index <= 0) {
-            return false;
-        } else {
-            return target.get(index).equals(target.get(index - 1));
-        }
-    }
-
-    static boolean isRightDuplicated(List<String> target, int index) {
-        if (index + 1 >= target.size()) {
-            return false;
-        } else {
-            return target.get(index).equals(target.get(index + 1));
-        }
+        String superReducedString = super_reduced_string("aaabccddd");
+        System.out.println(superReducedString);
     }
 
     // Complete the super_reduced_string function below.
     static String super_reduced_string(String s) {
-        List<String> target = Arrays.asList(s);
-        HashMap<String, Integer> map = new HashMap<>();
-        for(String str : target) {
-            if (map.get(str) == null) {
-                map.put(str, 1);
+        String[] split = s.split("");
+        Stack<String> stack = new Stack<>();
+        int idx = 0;
+        while (idx < split.length) {
+            String target = split[idx++];
+            if (stack.isEmpty()) {
+                stack.push(target);
             } else {
-                Integer integer = map.get(str);
-                map.put(str, ++integer);
+                if (stack.peek().equals(target)) {
+                    stack.pop();
+                } else {
+                    stack.push(target);
+                }
             }
         }
-
-        if (map.size() == 0) {
+        if (stack.isEmpty()) {
             return "Empty String";
         }
-        StringBuffer sb = new StringBuffer();
-        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
-        while(iterator.hasNext()) {
-            Map.Entry<String, Integer> next = iterator.next();
-            if (next.getValue() % 2 != 0) {
-                sb.append(next.getKey());
-            }
-        }
-        return sb.toString();
+        return String.join("", stack);
     }
 
 }
