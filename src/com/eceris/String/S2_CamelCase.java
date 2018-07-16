9aa5af737f154688aaed91c824b75287b2da5da5
eceris <eceris@gmail.com>
2018-07-16 21:47:42 +0900

    solve CamelCase

---
+++ src/com/eceris/String/S2_CamelCase.java
@@ -0,0 +1,27 @@
+package com.eceris.String;
+
+import java.util.concurrent.Callable;
+
+//    https://www.hackerrank.com/challenges/camelcase/problem
+public class S2_CamelCase {
+    public static void main(String[] args) {
+
+        String target = "saveChangesInTheEditor";
+        int camelcase = camelcase(target);
+        System.out.println(camelcase);
+    }
+
+    // Complete the camelcase function below.
+    static int camelcase(String s) {
+        int count = 0;
+        if (s.length() > 0) {
+            count++;
+        }
+        for (char c : s.toCharArray()) {
+            if (c < 96) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
