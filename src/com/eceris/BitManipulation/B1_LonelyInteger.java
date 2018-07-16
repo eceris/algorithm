8b2d7257b907f9578ba25dd5a4d870cf6b9de93f
eceris <eceris@gmail.com>
2018-07-16 18:38:16 +0900

    solve LonelyInteger

---
+++ src/com/eceris/BitManipulation/B1_LonelyInteger.java
@@ -0,0 +1,38 @@
+package com.eceris.BitManipulation;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+//    https://www.hackerrank.com/challenges/lonely-integer/problem
+public class B1_LonelyInteger {
+    public static void main(String[] args) {
+        int[] ints = {0, 0, 1, 2, 1};
+        int lonelyinteger = lonelyinteger(ints);
+        System.out.println(lonelyinteger);
+    }
+
+    static int lonelyinteger(int[] a) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i : a) {
+            Integer integer = map.get(i);
+            if (integer != null) {
+                map.put(i, ++integer);
+            } else {
+                map.put(i, 1);
+            }
+        }
+
+        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> next = iterator.next();
+            if (next.getValue() == 1) {
+                return next.getKey();
+            }
+        }
+        return 0;
+    }
+
+
+}
